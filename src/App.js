92c946f50222bfc3031c9766af12e7c08854d262
Sahil Turile <sahilturile@in.ibm.com>
2019-11-26 12:17:37 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Component1 from './component/Component1';
+
+function App() {
+  return (
+    <Component1/>
+  );
+}
+
+export default App;
---
+++ src/component/Component1.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+function Component1(){
+    return(
+      <p> Hello from Component1</p>
+    )
+}
+
+export default Component1;
